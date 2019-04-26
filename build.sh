fd397dfda63a99e1652b21a5b239a0cd872e7e4b
dungxop2405 <34085451+dungxop2405@users.noreply.github.com>
2019-04-26 14:31:35 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -17,6 +17,15 @@ tar -xvzf cmake.tar.gz
 cd cmake-3.1.1
 ./bootstrap
 make
+cd $BUILD_PATH
+wget https://github.com/glennrp/libpng/archive/v1.2.59.tar.gz
+tar -xvfz v1.2.59.tar.gz 
+cd libpng-1.2.59.tar/
+tar -xvfz libpng-1.2.59.tar
+cd libpng-1.2.59/libpng-1.2.59/
+./configure --prefix=/usr/local
+make
+make install
 
 cd $BUILD_PATH
 
